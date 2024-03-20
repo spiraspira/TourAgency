46d0d74ac70367aa2116397fb79755e03028b979
spiraspira <zabornik5350@gmail.com>
2024-03-21 02:45:31 +0300

    Add SaleValidator

---
+++ TourAgency.API/Validators/SaleValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace TourAgency.API.Validators;
+
+public class SaleValidator : AbstractValidator<SaleViewModel>
+{
+	public SaleValidator()
+	{
+		RuleFor(sale => sale.TravelPurpose)
+			.NotNull()
+			.Length(2, 150)
+			.WithMessage("Travel purpose must be between {MinLength} and {MaxLength} characters.");
+
+		RuleFor(sale => sale.SellDate)
+			.NotNull()
+			.LessThanOrEqualTo(DateTime.Now)
+			.WithMessage("Sell date cannot be in the future.");
+	}
+}
