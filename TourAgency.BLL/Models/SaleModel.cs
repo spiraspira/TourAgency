b87ef4cfe53ac732de176636f619877f8e7a9f71
spiraspira <zabornik5350@gmail.com>
2024-03-21 02:08:04 +0300

    Add SaleModel

---
+++ TourAgency.BLL/Models/SaleModel.cs
@@ -0,0 +1,12 @@
+﻿namespace TourAgency.BLL.Models;
+
+public class SaleModel : Model
+{
+	public string? TravelPurpose { get; set; }
+
+	public DateTime? SellDate { get; set; }
+
+	public Guid? RouteId { get; set; }
+
+	public RouteModel? Route { get; set; }
+}
