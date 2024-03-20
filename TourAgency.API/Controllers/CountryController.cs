be0fd4590d45c62ee74126ef0476dd12a19f1f35
spiraspira <zabornik5350@gmail.com>
2024-03-21 02:51:32 +0300

    Add CountryController

---
+++ TourAgency.API/Controllers/CountryController.cs
@@ -0,0 +1,46 @@
+﻿namespace TourAgency.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ActorController(
+	IMapper mapper,
+	IGenericService<CountryModel> countryService,
+	IValidator<CountryViewModel> validator)
+	: ControllerBase
+{
+	[HttpGet("{id}")]
+	public async Task<CountryViewModel> Get(Guid id)
+	{
+		return mapper.Map<CountryViewModel>(await countryService.Get(id));
+	}
+
+	[HttpGet]
+	public async Task<IEnumerable<CountryViewModel>> GetAll()
+	{
+		return mapper.Map<IEnumerable<CountryViewModel>>(await countryService.GetAll());
+	}
+
+	[HttpPost]
+	public async Task<CountryViewModel> Create(CountryViewModel model)
+	{
+		await validator.ValidateAndThrowAsync(model);
+
+		return mapper.Map<CountryViewModel>(
+			await countryService.Create(mapper.Map<CountryModel>(model)));
+	}
+
+	[HttpPut]
+	public async Task<CountryViewModel> Update(CountryViewModel model)
+	{
+		await validator.ValidateAndThrowAsync(model);
+
+		return mapper.Map<CountryViewModel>(
+			await countryService.Update(mapper.Map<CountryModel>(model)));
+	}
+
+	[HttpDelete]
+	public async Task<CountryViewModel> Delete(Guid id)
+	{
+		return mapper.Map<CountryViewModel>(await countryService.Delete(id));
+	}
+}
