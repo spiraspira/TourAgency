a4d4246777892ac8f8c4d0a7a487aad91cb1df7f
spiraspira <zabornik5350@gmail.com>
2024-03-20 16:25:38 +0300

    Add IGenericRepository

---
+++ TourAgency.DAL/Interfaces/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace TourAgency.DAL.Interfaces;
+
+public interface IGenericRepository<TEntity> where TEntity : IEntity
+{
+	Task<TEntity?> Get(Guid id);
+
+	Task<IEnumerable<TEntity>> GetAll();
+
+	Task<TEntity> Create(TEntity entity);
+
+	Task<TEntity> Update(TEntity entity);
+
+	Task<TEntity?> Delete(Guid id);
+}
