a1713410f7b36a62c6d97e28c7d3cf1d559374ec
spiraspira <zabornik5350@gmail.com>
2024-03-21 02:38:47 +0300

    Add CountryValidator

--- TourAgency.API/GlobalUsings.cs
+++ TourAgency.API/GlobalUsings.cs
@@ -1,5 +1,6 @@
 // Global using directives
 
 global using AutoMapper;
+global using FluentValidation;
 global using TourAgency.API.ViewModels;
 global using TourAgency.BLL.Models;
---
+++ TourAgency.API/Validators/CountryValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace TourAgency.API.Validators;
+
+public class CountryValidator : AbstractValidator<CountryViewModel>
+{
+	public CountryValidator()
+	{
+		RuleFor(country => country.Name)
+			.NotNull()
+			.Length(2, 56)
+			.WithMessage("Country name must be between {MinLength} and {MaxLength} characters.");
+
+		RuleFor(country => country.VisaPrice)
+			.NotNull()
+			.GreaterThanOrEqualTo(0)
+			.WithMessage("Visa price cannot have negative value.");
+	}
+}
