5bd535ea49b411721b4572ae1cd9d0c74684fe3c
spiraspira <zabornik5350@gmail.com>
2024-03-21 02:10:55 +0300

    Add AutoMapper to BLL

--- TourAgency.BLL/GlobalUsings.cs
+++ TourAgency.BLL/GlobalUsings.cs
@@ -1,3 +1,6 @@
 // Global using directives
 
+global using AutoMapper;
 global using TourAgency.BLL.Interfaces;
+global using TourAgency.BLL.Models;
+global using TourAgency.DAL.Entities;
---
+++ TourAgency.BLL/Mapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace TourAgency.BLL.Mapper;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<Country, CountryModel>().ReverseMap();
+
+		CreateMap<Route, RouteModel>().ReverseMap();
+
+		CreateMap<Sale, SaleModel>().ReverseMap();
+
+	}
+}
