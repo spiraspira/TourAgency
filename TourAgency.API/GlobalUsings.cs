8d1e6e6394dd0f90af1fdc0f11c8bc27810897e4
spiraspira <zabornik5350@gmail.com>
2024-03-21 02:33:57 +0300

    Add MappingProfile to API

---
+++ TourAgency.API/GlobalUsings.cs
@@ -0,0 +1,5 @@
+// Global using directives
+
+global using AutoMapper;
+global using TourAgency.API.ViewModels;
+global using TourAgency.BLL.Models;
---
+++ TourAgency.API/Mapper/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿namespace TourAgency.API.Mapper;
+
+public class MappingProfile : Profile
+{
+	public MappingProfile()
+	{
+		CreateMap<CountryModel, CountryViewModel>().ReverseMap();
+
+		CreateMap<RouteModel, RouteViewModel>().ReverseMap();
+
+		CreateMap<SaleModel, SaleViewModel>().ReverseMap();
+	}
+}
