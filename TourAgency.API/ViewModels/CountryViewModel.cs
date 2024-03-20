696bd6dbe9bc8ba8e10f03f83ebd57ebb569f644
spiraspira <zabornik5350@gmail.com>
2024-03-21 02:29:59 +0300

    Add CountryViewModel

---
+++ TourAgency.API/ViewModels/CountryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TourAgency.API.ViewModels;
+
+public class CountryViewModel
+{
+	public Guid? Id { get; set; }
+
+	public string? Name { get; set; }
+
+	public decimal? VisaPrice { get; set; }
+}
