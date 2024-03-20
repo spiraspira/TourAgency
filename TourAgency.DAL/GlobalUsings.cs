87129bcd4cac18570fa6787c0dbbd029d8661f33
spiraspira <zabornik5350@gmail.com>
2024-03-21 01:58:22 +0300

    Add DataAccessRegister

--- TourAgency.DAL/GlobalUsings.cs
+++ TourAgency.DAL/GlobalUsings.cs
@@ -1,5 +1,8 @@
 // Global using directives
 
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
 global using TourAgency.DAL.Entities;
 global using TourAgency.DAL.Interfaces;
+global using TourAgency.DAL.Repositories;
