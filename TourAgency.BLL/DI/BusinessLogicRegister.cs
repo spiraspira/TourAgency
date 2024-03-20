9a1190067a4c4a47afd16015e49d70a6af64e5b0
spiraspira <zabornik5350@gmail.com>
2024-03-21 02:27:08 +0300

    Add BusinessLogicRegister

---
+++ TourAgency.BLL/DI/BusinessLogicRegister.cs
@@ -0,0 +1,15 @@
+﻿namespace TourAgency.BLL.DI;
+
+public static class BusinessLogicRegister
+{
+	public static void AddBusinessLogic(this IServiceCollection services, IConfiguration config)
+	{
+		services.AddScoped<IGenericService<CountryModel>, GenericService<CountryModel, Country>>();
+
+		services.AddScoped<IGenericService<RouteModel>, GenericService<RouteModel, Route>>();
+
+		services.AddScoped<IGenericService<SaleModel>, GenericService<SaleModel, Sale>>();
+
+		services.AddDataAccess(config);
+	}
+}
