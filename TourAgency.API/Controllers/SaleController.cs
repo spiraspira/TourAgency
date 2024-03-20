5320729646d804f969896f79f57092c30675c9c6
spiraspira <zabornik5350@gmail.com>
2024-03-21 02:56:49 +0300

    Add SaleController

---
+++ TourAgency.API/Controllers/SaleController.cs
@@ -0,0 +1,46 @@
+﻿namespace TourAgency.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SaleController(
+	IMapper mapper,
+	IGenericService<SaleModel> saleService,
+	IValidator<SaleViewModel> validator)
+	: ControllerBase
+{
+	[HttpGet("{id}")]
+	public async Task<SaleViewModel> Get(Guid id)
+	{
+		return mapper.Map<SaleViewModel>(await saleService.Get(id));
+	}
+
+	[HttpGet]
+	public async Task<IEnumerable<SaleViewModel>> GetAll()
+	{
+		return mapper.Map<IEnumerable<SaleViewModel>>(await saleService.GetAll());
+	}
+
+	[HttpPost]
+	public async Task<SaleViewModel> Create(SaleViewModel model)
+	{
+		await validator.ValidateAndThrowAsync(model);
+
+		return mapper.Map<SaleViewModel>(
+			await saleService.Create(mapper.Map<SaleModel>(model)));
+	}
+
+	[HttpPut]
+	public async Task<SaleViewModel> Update(SaleViewModel model)
+	{
+		await validator.ValidateAndThrowAsync(model);
+
+		return mapper.Map<SaleViewModel>(
+			await saleService.Update(mapper.Map<SaleModel>(model)));
+	}
+
+	[HttpDelete]
+	public async Task<SaleViewModel> Delete(Guid id)
+	{
+		return mapper.Map<SaleViewModel>(await saleService.Delete(id));
+	}
+}
