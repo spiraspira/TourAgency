e48d3d98f91ee8ed36cddc848d2cdae615646674
spiraspira <zabornik5350@gmail.com>
2024-03-20 16:11:38 +0300

    Add Sale entity

---
+++ TourAgency.DAL/Entities/Sale.cs
@@ -0,0 +1,12 @@
+﻿namespace TourAgency.DAL.Entities;
+
+public class Sale : Entity
+{
+	public string? TravelPurpose { get; set; }
+
+	public DateTime? SellDate { get; set; }
+
+	public Guid? RouteId { get; set; }
+
+	public Route? Route { get; set; }
+}
