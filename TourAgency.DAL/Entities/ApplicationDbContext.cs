22f353cda2a539adfc52631d72ca59f138a4dbd5
spiraspira <zabornik5350@gmail.com>
2024-03-20 16:14:20 +0300

    Add ApplicationDbContext

---
+++ TourAgency.DAL/Entities/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TourAgency.DAL.Entities;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+	public DbSet<Country>? Countries { get; set; }
+
+	public DbSet<Route>? Routes { get; set; }
+
+	public DbSet<Sale>? Sales { get; set; }
+}
