afea9dda27cdc528824a8d8d2119968a36c795a5
spiraspira <zabornik5350@gmail.com>
2024-03-21 10:00:56 +0300

    Add Program.cs (services, middleware, logger)

--- TourAgency.API/GlobalUsings.cs
+++ TourAgency.API/GlobalUsings.cs
@@ -2,7 +2,13 @@
 
 global using AutoMapper;
 global using FluentValidation;
+global using Microsoft.AspNetCore.Diagnostics;
 global using Microsoft.AspNetCore.Mvc;
+global using Newtonsoft.Json;
+global using Serilog;
+global using Serilog.Events;
+global using System.Reflection;
 global using TourAgency.API.ViewModels;
+global using TourAgency.BLL.DI;
 global using TourAgency.BLL.Interfaces;
 global using TourAgency.BLL.Models;
---
+++ TourAgency.API/Program.cs
@@ -0,0 +1,30 @@
+﻿Log.Logger = new LoggerConfiguration()
+	.MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+	.Enrich.FromLogContext()
+	.WriteTo.Console()
+	.CreateLogger();
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAutoMapper(typeof(TourAgency.API.Mapper.MappingProfile), typeof(TourAgency.BLL.Mapper.MappingProfile));
+builder.Services.AddBusinessLogic(builder.Configuration);
+builder.Services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSerilog();
+builder.Services.AddSwaggerGen();
+builder.Services.AddValidatorsFromAssembly(Assembly.Load("TourAgency.API"));
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
