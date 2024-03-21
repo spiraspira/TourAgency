952b1f25958cf159d4d516fa2a5f6c4eb007fb73
spiraspira <zabornik5350@gmail.com>
2024-03-22 01:57:51 +0300

    Add CountryTable

--- TourAgency.Client/src/components/pages/CountriesPage.jsx
+++ TourAgency.Client/src/components/pages/CountriesPage.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Footer from '../header/Footer';
 import Header from '../header/Header';
+import CountryTable from './tables/CountryTable';
 
 const CountriesPage = () => {
   return (
     <div>
       <Header />  
+      <CountryTable />
       <Footer />
     </div>
   );
