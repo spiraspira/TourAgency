320f628b6143708460f9e835c05877c4012c2b8c
spiraspira <zabornik5350@gmail.com>
2024-03-22 12:45:50 +0300

    Add RouteTable

--- TourAgency.Client/src/components/pages/RoutesPage.jsx
+++ TourAgency.Client/src/components/pages/RoutesPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Footer from '../header/Footer';
+import RouteTable from './tables/RouteTable';
 import Header from '../header/Header';
 
 const RoutesPage = () => {
   return (
     <div>
       <Header />  
+      <RouteTable />
       <Footer />
     </div>
   );
