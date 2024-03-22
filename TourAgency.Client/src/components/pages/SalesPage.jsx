a35007242e9a0d984e10ba7fbc9afe549fd22a11
spiraspira <zabornik5350@gmail.com>
2024-03-22 12:48:24 +0300

    Fix RouteTable title

--- TourAgency.Client/src/components/pages/SalesPage.jsx
+++ TourAgency.Client/src/components/pages/SalesPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Footer from '../header/Footer';
+import SaleTable from './tables/SaleTable';
 import Header from '../header/Header';
 
 const SalesPage = () => {
   return (
     <div>
       <Header />  
+      <SaleTable />
       <Footer />
     </div>
   );
